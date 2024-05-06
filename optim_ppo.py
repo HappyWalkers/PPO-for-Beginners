dea53b91b3077955833367711a7fb0314a396fb2
Yunpeng <mashed.potato.mine@gmail.com>
2024-05-05 22:39:02 -0400

    load the model if it exists

--- optim_ppo.py
+++ optim_ppo.py
@@ -4,6 +4,8 @@ from part4.ppo_for_beginners.network import FeedForwardNN
 from dm_control import suite,viewer
 import numpy as np
 from dmcontrol_to_gym_wrapper import DmcontrolToGymWrapper
+import torch
+import sys
 
 def main(args: argparse.Namespace):
     hyperparameters = {
@@ -14,7 +16,7 @@ def main(args: argparse.Namespace):
         'lr': 5e-3, 
         'lam': 0.98,
         'clip': 0.2, 
-        'save_freq': 1e15, 
+        'save_freq': 500, 
         'seed': args.seed,
         'max_grad_norm':0.5,
         'target_kl':0.02,
@@ -30,13 +32,33 @@ def main(args: argparse.Namespace):
 
     if args.train:
         model = PPO(FeedForwardNN, env, **hyperparameters)
+        
+        # Tries to load in an existing actor/critic model to continue training on
+        actor_model, critic_model = args.actor_model, args.critic_model
+        if actor_model != '' and critic_model != '':
+            print(f"Loading in {actor_model} and {critic_model}...", flush=True)
+            model.actor.load_state_dict(torch.load(actor_model))
+            model.critic.load_state_dict(torch.load(critic_model))
+            print(f"Successfully loaded.", flush=True)
+        elif actor_model != '' or critic_model != '': # Don't train from scratch if user accidentally forgets actor/critic model
+            print(f"Error: Either specify both actor/critic models or none at all. We don't want to accidentally override anything!")
+            sys.exit(0)
+        else:
+            print(f"Training from scratch.", flush=True)
+            
         model.learn(total_timesteps)
+        
+    # if args.test:
+    #     pass
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=('Train an agent using PPO.'))
     parser.add_argument('--train', action='store_true', help='Train the agent.', default=True)
+    # parser.add_argument('--test', action='store_false', help='Test the agent.', default=False)
     parser.add_argument('--seed', dest='seed', type=int, default=None)
+    parser.add_argument('--actor_model', dest='actor_model', type=str, default='')
+    parser.add_argument('--critic_model', dest='critic_model', type=str, default='')
     args = parser.parse_args()
     main(args)
